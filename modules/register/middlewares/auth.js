e3bc163f1ce03c83d26b518f7227115109c9aa07
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-08 23:39:23 +0300

    Update auth middleware

--- modules/register/middlewares/auth.js
+++ modules/register/middlewares/auth.js
@@ -8,9 +8,7 @@ module.exports = [
 ]
 
 function authenticate(req, res, next) {
-  //const access_token = req.cookies.access_token || req.get('Authorization') || null;
-  const authorization = req.get('Authorization');
-  const access_token = authorization.split('Bearer ')[1];
+  const access_token = req.get('Authorization') || null;
 
   if(access_token == null) {
     res.status(401).send('access_token_not_found');
@@ -28,7 +26,7 @@ function authenticate(req, res, next) {
 }
 
 function getUser(req, res, next) {
-  let collection = req.app.get('DB').collection('register');
+  let collection = req.app.get('DB').collection('users');
   let filter = {_id: ObjectID(req.user._id)};
 
   let p = collection.findOne(filter);
@@ -36,7 +34,7 @@ function getUser(req, res, next) {
     if(result) {
       req.user = result;
       next();
-    } else return res.status(400).send('user__not_found');
+    } else return res.status(400).send('user_not_found');
   }).catch(error => {
     console.log(error);
     res.sendStatus(500);

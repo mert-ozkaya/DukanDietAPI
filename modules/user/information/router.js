126ff164e21d4f49ba2e0034ab6fac7e635ada46
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-08 23:34:17 +0300

    Remove user module

--- modules/user/information/router.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router;
-
-const router = Router();
-
-router.get('/get', require('./callbacks/get-info'));
-router.post('/post', require('./callbacks/post-info'))
-module.exports = router;
--- modules/user/router.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router;
-
-const router = Router();
-
-router.use('/information', require('./information/router'));
-
-module.exports = router;

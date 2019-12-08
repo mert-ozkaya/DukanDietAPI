624f3ceeab0cc370127fc1b15c424757df0674d1
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-08 23:39:03 +0300

    Update \register\router: Add /check endpoint

--- modules/register/router.js
+++ modules/register/router.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router;
 
+
 const router = Router();
 
 router.post('/sign-up', require('./callbacks/sign-up'))
 router.post('/login', require('./callbacks/login'))
+router.post('/check', require('./middlewares/auth'), require('./callbacks/auth-check'));
 
 module.exports = router;

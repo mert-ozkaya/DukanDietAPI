a3d8d3029949d110b2cd54d826e8b5a2ad1d9bae
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-09 22:03:38 +0300

    Update activities\router: add list and update endpoint

--- modules/activities/router.js
+++ modules/activities/router.js
@@ -3,6 +3,6 @@ const router = Router();
 const _auth = require('../register/middlewares/auth')
 
 router.get('/', _auth, require('./get-activity'))
-
-
+router.get('/list', _auth, require('./list'))
+router.post('/update/:_id',_auth,require('./update_activity'))
 module.exports = router;

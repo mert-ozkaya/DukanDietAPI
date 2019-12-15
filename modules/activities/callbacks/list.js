61e43a19efd981cd83f598ca9d5335a17212b068
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-15 17:26:40 +0300

    Update activities list.js: add project to aggregate pipeline

--- modules/activities/callbacks/list.js
+++ modules/activities/callbacks/list.js
@@ -8,9 +8,17 @@ module.exports = function(req,res) {
       '$match': {
         'user_id': ObjectID(user_id)
       }
+    },
+    {
+      '$project': {
+        '_id': 1,
+        'date': 1,
+        'phase': 1,
+        'foodCategory': 1
+      }
     }
   ]
-
+  console.log("list")
   let collection = req.app.get('DB').collection('activities');
   let cursor = collection.aggregate(pipeline)
   let p = cursor.toArray()
@@ -18,13 +26,9 @@ module.exports = function(req,res) {
   p.then((result) => {
 
     if(result) res.send(result)
-    else res.sendStatus(400)
+    else res.status(400).send("Kayıt başarısız")
 
   }).catch((err) => {
     res.status(500).send('db-error');
   })
-
-
-
-
 }

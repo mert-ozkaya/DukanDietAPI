1744d0d45dc017d16e722ec65b3436b4b456e5de
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-15 17:27:34 +0300

    Update get-activity: update respoonse data

--- modules/activities/callbacks/get-activity.js
+++ modules/activities/callbacks/get-activity.js
@@ -4,6 +4,7 @@ const moment = require('moment')
 
 module.exports = function(req,res) {
 
+    //console.log(req.body
     let user_id = ObjectID(req.user._id)
 
     let pipeline = [
@@ -29,21 +30,28 @@ module.exports = function(req,res) {
     let cursor = collection.aggregate(pipeline)
     let p = cursor.toArray()
 
-    p.then((result) => {
 
+    p.then((result) => {
       let data = {
+        _id: result[0].activity._id,
         name: result[0].given_name,
         date: result[0].activity.date,
         weight: result[0].weight,
+        weightOfDay: result[0].activity.weightOfDay,
         physical_activity: result[0].activity.physicalActivities,
         phase: result[0].activity.phase,
         menu: result[0].activity.menu,
-        lost_weight: result[0].lost_weight, //kaybettiği kilo
-        verilecek_kilo: result[0].weight - result[0].lost_weight // kaybedeceği kilo
+        kaybettigi_kilo: parseInt(result[0].lost_weight), //kaybettiği kilo
+        kaybedecegi_kilo: parseInt(result[0].toplamVerilecekKilo - result[0].lost_weight), // kaybedeceği kilo
+        foodCategory: result[0].activity.foodCategory,
+        chest: result[0].activity.measurements.chest,
+        waist: result[0].activity.measurements.waist,
+        buttocks: result[0].activity.measurements.buttocks,
+        note: result[0].activity.notes
       }
 
       let new_date = data.date.split('-')
-      data.date = new_date[2]+'.'+ new_date[1] + '.' + new_date[0]
+      data.date = new_date[2] + '/' + new_date[1] + '/' + new_date[0]
 
       if(result) res.send(data)
       else res.sendStatus(400)

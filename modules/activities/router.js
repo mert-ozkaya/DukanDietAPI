1e5880567fbd4a256f2dfaf72d7231861b00c93a
Mert Ozkaya <40736764+mert-ozkaya@users.noreply.github.com>
2019-12-25 22:18:26 +0300

    Update activities: Add details endpoint

--- modules/activities/router.js
+++ modules/activities/router.js
@@ -2,7 +2,13 @@ const Router = require('express').Router;
 const router = Router();
 const _auth = require('../register/middlewares/auth')
 
+
+
 router.get('/', _auth, require('./callbacks/get-activity'))
+router.get('/details/:_id', require('./callbacks/activity-detail'))
 router.get('/list', _auth, require('./callbacks/list'))
 router.post('/update/:_id([a-fA-F0-9]{24})',_auth, require('./callbacks/update_activity'))
+
+
+
 module.exports = router;
